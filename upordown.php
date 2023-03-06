ceabbed8273104460c6a94bc5bfce03e9be4128a
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-06 08:50:08 +0100

    Update upordown.php

--- upordown.php
+++ upordown.php
@@ -1,37 +1,41 @@
 <?php
 function upordown($url)
 {
-    $cs= curl_init($url=null);
+    $cs = curl_init($url);
     curl_setopt($cs, CURLOPT_NOBODY, TRUE);
     curl_setopt($cs, CURLOPT_FOLLOWLOCATION, TRUE);
-    $status_code=  curl_getinfo($cs,CURLINFO_HTTP_CODE);
-    return($status_code=200)?true:FALSE;
+    $status_code = curl_getinfo($cs, CURLINFO_HTTP_CODE);
+    return $status_code == 200;
 }
-if(isset($_POST['url'])==TRUE && empty($_POST['url'])==FALSE)
+
+if(isset($_POST['url']) && !empty($_POST['url']))
 {
-    $url=  trim($_POST['url']);
-    if(filter_var($url,FILTER_VALIDATE_URL)==true)
+    $url = trim($_POST['url']);
+    if(filter_var($url, FILTER_VALIDATE_URL))
     {
-        if(upordown($url)==TRUE)
+        if(upordown($url))
         {
-        echo 'Wensite url is up';
-    }  else {
-       echo 'sorry, Website url id down'; 
+            echo 'Website URL is up';
+        }  
+        else 
+        {
+            echo 'Sorry, Website URL is down'; 
+        }
     }
-    }
-   
- else {
-        echo 'Invalid Url'; 
-    }
-    
- }
+    else 
+    {
+        echo 'Invalid URL'; 
+    }   
+}
 ?>
+
 <!doctype html>
 <html>
     <body>
         <form action="" method="post">
-          Up or Down?  <input  type="text" name="url" placeholder="search">
-            <input  type="submit">
+          <label for="url">Website URL:</label>
+          <input type="text" id="url" name="url" placeholder="Enter URL">
+          <input type="submit" value="Check">
         </form>
     </body>
 </html>
