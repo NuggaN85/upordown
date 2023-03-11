5c5431cec4061ba13175da893888d51d4337953b
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-11 20:11:24 +0100

    Update upordown.php

--- upordown.php
+++ upordown.php
@@ -1,41 +1,37 @@
 <?php
-function upordown($url)
-{
-    $cs = curl_init($url);
-    curl_setopt($cs, CURLOPT_NOBODY, TRUE);
-    curl_setopt($cs, CURLOPT_FOLLOWLOCATION, TRUE);
-    $status_code = curl_getinfo($cs, CURLINFO_HTTP_CODE);
-    return $status_code == 200;
+function checkWebsiteStatus($url) {
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_NOBODY, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_exec($curl);
+    $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+    return $statusCode;
 }
 
-if(isset($_POST['url']) && !empty($_POST['url']))
-{
+if(isset($_POST['url']) && !empty($_POST['url'])) {
     $url = trim($_POST['url']);
-    if(filter_var($url, FILTER_VALIDATE_URL))
-    {
-        if(upordown($url))
-        {
+    if(filter_var($url, FILTER_VALIDATE_URL)) {
+        $statusCode = checkWebsiteStatus($url);
+        if($statusCode == 200) {
             echo 'Website URL is up';
-        }  
-        else 
-        {
+        } else {
             echo 'Sorry, Website URL is down'; 
         }
-    }
-    else 
-    {
+    } else {
         echo 'Invalid URL'; 
     }   
 }
 ?>
-
 <!doctype html>
 <html>
     <body>
         <form action="" method="post">
-          <label for="url">Website URL:</label>
-          <input type="text" id="url" name="url" placeholder="Enter URL">
-          <input type="submit" value="Check">
+            <label for="url">Website URL:</label>
+            <input type="text" id="url" name="url" placeholder="Enter URL">
+            <input type="submit" value="Check">
         </form>
     </body>
 </html>
